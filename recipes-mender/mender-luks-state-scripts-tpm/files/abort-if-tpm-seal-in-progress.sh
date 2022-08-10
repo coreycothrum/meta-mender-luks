d877cc941e2c051001e1832b3a42cc1ba719a1b0
corey cothrum <contact@coreycothrum.com>
2022-08-10 13:39:25 -0500

    feat: abort mender update if TPM is in process of being sealed
    add new state script to check if mender-luks-tpm-seal-on-boot.service is
    active and abort if it is.
    
    this is a safety precaution to make sure TPM is at rest before an update
    happens and potentially tries to unseal it.

---
+++ recipes-mender/mender-luks-state-scripts-tpm/files/abort-if-tpm-seal-in-progress.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+function log {
+  echo "$@" >&2
+}
+log "$(cat /etc/mender/artifact_info): $(basename "$0") was called!"
+
+function fatal {
+  log $@
+  exit 1
+}
+
+################################################################################
+if systemctl --quiet is-active mender-luks-tpm-seal-on-boot.service > /dev/null 2>&1; then
+  fatal "mender-luks-tpm-seal-on-boot.service is still active. Wait longer before you can update again."
+fi
+
+exit
