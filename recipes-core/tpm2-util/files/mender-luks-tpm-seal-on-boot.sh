0add2ccfaac647ac3d1ef71d20409891f16e3387
corey cothrum <contact@coreycothrum.com>
2022-08-24 12:55:12 -0500

    refactor: mender-luks-tpm-seal-on-boot.service type=oneshot
    refactor mender-luks-tpm-seal-on-boot.service for more reliability. The
    new ordering added is better suited w/ these changes:
      * make type=oneshot
      * check explicitly for failed systemd services (not is-system-running)
    
    This better lets us set explicit dependencies w/ MENDER/LUKS_SEAL_SYSTEMD_AFTER

--- recipes-core/tpm2-util/files/mender-luks-tpm-seal-on-boot.sh
+++ recipes-core/tpm2-util/files/mender-luks-tpm-seal-on-boot.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
-echo "will check status in @@MENDER/LUKS_SEAL_DELAY_SECS@@ seconds"
-
-sleep @@MENDER/LUKS_SEAL_DELAY_SECS@@
-
 rc=0
 
-if systemctl --quiet is-system-running; then
+if ! systemctl --quiet is-failed "*"; then
   echo "sealing TPM PCRS to max: @@MENDER/LUKS_TPM_PCR_SET_MAX@@"
   mender-luks-tpm2-util.sh --write --pcrs max
   rc=$?
 else
   echo "System may be unstable, not sealing TPM PCRs for fear of bricking system"
+  rc=1
 fi
 
 exit $rc
