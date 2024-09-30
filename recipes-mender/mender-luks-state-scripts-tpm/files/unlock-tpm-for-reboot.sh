31f5f4c0b340f5f0cc1455cb977d285e63c9e53f
Corey Cothrum <contact@coreycothrum.com>
2024-09-30 11:12:07 +0000

    feat(mender-luks-state-scripts-tpm): add mender-luks-cryptenroll unlock/unseal
    for now, do both... so we can later migrate to the new method and drop this completely

--- recipes-mender/mender-luks-state-scripts-tpm/files/unlock-tpm-for-reboot.sh
+++ recipes-mender/mender-luks-state-scripts-tpm/files/unlock-tpm-for-reboot.sh
@@ -7,7 +7,15 @@ function log {
 log "$(cat /etc/mender/artifact_info): $(basename "$0") was called!"
 
 function cleanup {
-  @@sbindir@@/mender-luks-tpm2-util.sh --write --pcrs @@MENDER/LUKS_TPM_PCR_UPDATE_UNLOCK@@
+  if command -v @@sbindir@@/mender-luks-tpm2-util.sh; then
+    log "$(cat /etc/mender/artifact_info): mender-luks-tpm2-util.sh"
+             @@sbindir@@/mender-luks-tpm2-util.sh   --write --pcrs @@MENDER/LUKS_TPM_PCR_UPDATE_UNLOCK@@
+  fi
+
+  if command -v @@sbindir@@/mender-luks-cryptenroll.sh; then
+    log "$(cat /etc/mender/artifact_info): mender-luks-cryptenroll.sh"
+             @@sbindir@@/mender-luks-cryptenroll.sh -u
+  fi
 }
 trap cleanup EXIT
 
