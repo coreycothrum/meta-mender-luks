2f0504b5ce853cee6b673d0a745bd2f69e2b7020
Corey Cothrum <contact@coreycothrum.com>
2024-10-31 13:32:13 +0000

    feat(mender-luks-state-scripts): add cryptenroll based TPM seal/unseal
    if new cryptenroll scripts exist, use them

--- recipes-mender/mender-luks-state-scripts/files/tpm-seal.sh
+++ recipes-mender/mender-luks-state-scripts/files/tpm-seal.sh
@@ -8,8 +8,10 @@ log "$(cat /etc/mender/artifact_info): $(basename "$0") was called!"
 
 function cleanup {
   local CMD="@@sbindir@@/mender-luks-tpm2-util.sh"
-
   command -v "${CMD}" && [[ -f "@@MENDER/LUKS_LEGACY_KEY_FILE@@" ]] && ${CMD} --write --pcrs max
+
+  local CMD="@@sbindir@@/mender-luks-cryptenroll.sh"
+  command -v "${CMD}" && ${CMD} -t "@@MENDER/LUKS_CRYPTENROLL_TPM2_SEALED_PCRS@@"
 
   return 0
 }
