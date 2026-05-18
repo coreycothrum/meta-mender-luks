d786873f5e06fc7edc7a10e4a1429af22162114e
Corey Cothrum <contact@coreycothrum.com>
2026-05-18 06:28:38 -0500

    refactor: remove "mender-update" show/print from state scripts
    this call/print is/was a breaking change between mender client major versions...
    
    ... removing it to maintain backwards compatibility

--- recipes-mender/mender-luks-state-scripts-tpm/files/tpm-seal.sh
+++ recipes-mender/mender-luks-state-scripts-tpm/files/tpm-seal.sh
@@ -4,7 +4,6 @@ set -e
 function log {
   echo "$@" >&2
 }
-log "$(mender-update show-artifact): $(basename "$0") was called!"
 
 function cleanup {
   @@sbindir@@/mender-luks-tpm2-util.sh --write --pcrs max
